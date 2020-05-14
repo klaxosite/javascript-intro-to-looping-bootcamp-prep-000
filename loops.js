594da9327dff9c140c1cc55fa56a03f6059b74d3
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 05:36:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,13 +17,14 @@ function whileLoop(n){
 
 }
 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+
 
 function doWhileLoop(num){
-
+  let i = 0
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do{
     console.log("I run once regardless.")
   }while(incrementVariable() < num)
